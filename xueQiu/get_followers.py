8e968a42f81a7a15a9640ee370268708e82b492c
crocodile3 <yrt333333@163.com>
2018-11-15 22:50:29 +0800

    完善关注列表跟粉丝列表

--- xueQiu/get_followers.py
+++ xueQiu/get_followers.py
@@ -39,7 +39,7 @@ def get_userid_from_mysql():
     start_num = 160000
     
     # 总共多少数据
-    allData = 2000000
+    allData = 4000000
     # 每个批次多少条数据
     dataOfEach = 1000
     # 批次
@@ -55,7 +55,6 @@ def get_userid_from_mysql():
     
 
 def get_self_info(user_id):
-    #todo 做去重判断
     exist_status = redis_client.sismember(user_set,user_id)
     if not exist_status:
         try:
@@ -70,6 +69,7 @@ def get_self_info(user_id):
             fan_count = re.search(r'id="fan_count">(.*?)<', html).group(1) if re.search(r'id="fan_count">(.*?)<', html) else None
             self_introduce = re.search(r'个人介绍：(.*?)<', html).group(1) if re.search(r'个人介绍：(.*?)<', html) else None
             timeStamp = re.search(r'data-age="(.*?)">', html).group(1) if re.search(r'data-age="(.*?)">', html) else None
+            place = re.search(r'<span>所在地区：(.*?)</span>',html).group(1) if re.search(r'<span>所在地区：(.*?)</span>',html) else None
             if timeStamp:
                 timeStamp = int(timeStamp) / 1000
                 date = time.localtime(timeStamp)
@@ -77,8 +77,9 @@ def get_self_info(user_id):
             else:
                 birth = None
             weibo = re.search(r'<a href="(.*?)" class="u-slg u-slg-sn" title="新浪微博"', html).group(1) if ('新浪微博' in html and re.search(r'<a href="(.*?)" class="u-slg u-slg-sn" title="新浪微博"', html)) else None
-            # todo 豆瓣
-            douban = ''
+            douban = re.search(r'<a href="(.*?)" class="u-slg u-slg-db" title="豆瓣网"', html).group(1) if ('豆瓣' in html and re.search(r'<a href="(.*?)" class="u-slg u-slg-db" title="豆瓣网"', html)) else None
+            fans = get_followeds(user_id,fan_count)
+            follows = get_follows(user_id,follow_count)
             self_info = dict(
                 user_id=user_id,
                 name=name,
@@ -89,16 +90,22 @@ def get_self_info(user_id):
                 self_introduce=self_introduce,
                 birth=birth,
                 weibo=weibo,
-                douban=douban
+                douban=douban,
+                place = place,
+                follows=follows,
+                fans=fans
             )
-            with open('user_info.csv','a',encoding='utf8') as csvFile:
-                fieldnames = [key for key in self_info.keys()]
-                # f.write(self_info)
-                csvWriter = csv.DictWriter(csvFile,fieldnames=fieldnames)
-                csvWriter.writerow(self_info)
-                redis_client.sadd(user_set,user_id)
-            time.sleep(random.uniform(0.3, 2))
-            return follow_count,fan_count
+            print(self_info)
+            if name is not None:
+                with open('user_infoV1.csv','a',encoding='utf8') as csvFile:
+                    fieldnames = [key for key in self_info.keys()]
+                    print(fieldnames)
+                    # f.write(self_info)
+                    csvWriter = csv.DictWriter(csvFile,fieldnames=fieldnames)
+                    csvWriter.writerow(self_info)
+                    redis_client.sadd(user_set,user_id)
+                time.sleep(random.uniform(0.3, 1))
+                return follow_count,fan_count
         except Exception as e:
             with open('fail_users.txt','a') as f:
                 f.write(user_id+'\n')
@@ -113,6 +120,7 @@ def get_follows(userid,follow_count):
         userid)
     pages = int(follow_count)//20+1
     print('获取{}的关注人,总计{}页'.format(userid, pages))
+    followlist = []
     for page in range(pages):
         info = {'name': 'follows', 'page': page, 'uid': userid}
         params, encSecKey = get_paramsv1(info)
@@ -122,8 +130,10 @@ def get_follows(userid,follow_count):
         follows = data.get('follow')
         if follows:
             for follow in follows:
-                print(follow)
+                follow_dict = {'nickname':follow['nickname'],'useid':follow['userId']}
+                followlist.append(follow_dict)
         print('获取第{}页关注人完成'.format(page + 1))
+    return followlist
 
 
 def get_followeds(userid,fan_count):
@@ -135,31 +145,30 @@ def get_followeds(userid,fan_count):
     followeds_url = 'https://music.163.com/weapi/user/getfolloweds?csrf_token=e5d4805ff4c96f57d85c1b481b30c121'
     pages = int(fan_count) // 20 + 1
     print('获取{}的粉丝,总计{}页'.format(userid,pages))
+    fanslist = []
     for page in range(pages):
         info = {'name': 'followeds', 'page': page, 'uid': userid}
         params, encSecKey = get_paramsv1(info)
         data = {'params': params, 'encSecKey': encSecKey}
         resp = requests.post(followeds_url, headers=header, data=data)
         data = json.loads(resp.text)
+        print(data)
         fans = data.get('followeds')
         if fans:
             for fan in fans:
-                print(fan)
+                fan_dict = {'nickname':fan['nickname'],'useid':fan['userId']}
+                fanslist.append(fan_dict)
         print('获取第{}页粉丝完成'.format(page+1))
+    return fanslist
 
 
 def get_user_info():
-    items = get_userid_from_mysql()
-    [get_self_info(userid[0]) for item in items for userid in item]
-    # for item in items:
-    #     for row in item:
-    #         print(row[0])
-    # [get_self_info(userid[0]) for userid in userids]
-        # 现获取用户信息表（里面有关注数和粉丝数）
-        # follow_count,fan_count = get_self_info(userid)
-        # 获取关注列表
-        # get_follows(userid,follow_count)
-        # 获取粉丝列表
-        # get_followeds(userid,fan_count)
+    userid = '375576'
+    fan_count = 27
+    get_followeds(userid, fan_count)
+    # items = get_userid_from_mysql()
+    # [get_self_info(userid[0]) for item in items for userid in item]
+    
+
 if __name__ == "__main__":
     get_user_info()
